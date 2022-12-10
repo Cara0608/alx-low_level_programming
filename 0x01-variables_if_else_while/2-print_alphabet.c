e694d7d3b6427cb8039a119eb721fa9c9b770071
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-10 20:23:54 +0100

    Alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - prints alphabet 
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
