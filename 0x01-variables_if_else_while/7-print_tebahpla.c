e711bb852de1a2e6ddb319d8bad431e2c0cacd20
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-10 21:36:12 +0100

    TEBAHPLA

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - serves as entry point of the program
+ *
+ * Return:By default returns 0 if no errors otherwise returns a non-zero value
+ *
+ */
+int main(void)
+{
+	char i;
+	for (i = 'z'; i >= 'a'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
