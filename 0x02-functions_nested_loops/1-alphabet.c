bc19e534514f6630f87c0db288f8cd135ed260e7
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-13 17:46:16 +0100

    Putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - serves as entry point for the program
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+void print_alphabet(void);
+{
+
