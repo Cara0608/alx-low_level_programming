10690dcbef227c3dd9cd64d9e0b9fd48eb4f0ec2
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-14 21:45:19 +0100

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - prints the sign of a number
  * @n: The number of the sign to be printed
- * Return: 1 if number is greater thanzero
+ * Return: 1 if number is greater than zero
  * 0 if number is zero
  * -1 if number is less than zero
  */
