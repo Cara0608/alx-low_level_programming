0062f649f860e504dc3421b684f39981059a9a7a
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-16 16:16:34 +0100

    24 hrs

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test function that prints the remaining day
+ * Return; 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	print_remaining_days(i);
+
+	return (0)
+}
