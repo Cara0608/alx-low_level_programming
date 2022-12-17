0c88d9a4365437704194004f62db99c1868fa33b
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-17 11:01:22 +0100

    More functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 nums
+ * @a: first param
+ * @b: second param
+ * Return: something but success
+ */
+
+int main(int a, int b)
+{
+	return (a * b);
+}
