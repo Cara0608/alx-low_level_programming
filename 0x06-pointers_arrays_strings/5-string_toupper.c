d145daf0007f4f3602123362505981f00a7ddbfd
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-23 09:24:55 +0100

    Pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,16 +8,14 @@
  */
 char *string_toupper(char *p)
 {
-  int a = 0;
-
-  while (p[a])
-  {
-	  if (p[a] >= 97 && p[a] <= 122)
-	  {
-		  p[a] -= 32;
-	  }
-	  a++;
-  }
-
-  return (p);
+	int a = 0;
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
 }
