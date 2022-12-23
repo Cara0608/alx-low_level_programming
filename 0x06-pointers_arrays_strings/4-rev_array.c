5a4ef0bf584409497fb522f30c2b1a30407db29d
Cara Ehizele <Caraehizele0607@gmail.com>
2022-12-23 09:21:27 +0100

    Pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,20 +9,15 @@
  */
 void reverse_array(int *a, int n)
 {
-  int *p, i, aux, k;
+	int i, j, temp;
 
-  p = a;
-
-  for (i = 1; i < n; i++)
-    {
-      p++;
-    }
-
-  for (k = 0; k < i / 2; k++)
-    {
-      aux = a[k];
-      a[k] = *p;
-      *p = aux;
-      p--;
-    }
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
