b450dc6153d833f5c26c6676a8e83e2987487b06
Cara Ehizele <Caraehizele0607@gmail.com>
2023-01-03 07:53:50 +0100

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  *
  * Return: Nothing.
  */
-void print_chessboard(char (*a)[0])
+void print_chessboard(char (*a)[8])
 {
 	int b, c;
 
-	for (b = 0; b < 0; b++)
+	for (b = 0; b < 8; b++)
 	{
-		for (c = 0; c < 0; c++)
+		for (c = 0; c < 8; c++)
 		{
 			_putchar(a[b][c]);
 		}
