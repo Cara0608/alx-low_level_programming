ce2bf3678b895342237d90c39dcbf023b980a94d
Cara Ehizele <Caraehizele0607@gmail.com>
2023-01-04 21:36:28 +0100

    Advanced tasks

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,8 @@ int _pow_recursuon(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+void _print_rev_recursion(char *s);
+int wildcmp(char *s1, char *s2);
+int _strlen_recursion(char *s);
 
 #endif
