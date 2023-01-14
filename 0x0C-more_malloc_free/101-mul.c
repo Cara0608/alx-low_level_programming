68797dc86fa1b4f8c482ed52c15246ff672f63c7
Cara Ehizele <Caraehizele0607@gmail.com>
2023-01-14 13:53:34 +0100

    More mallocs

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -131,7 +131,9 @@ int main(int argc, char *argv[])
 	}
 
 	for (len1 = 0; argv[1][len1]; len1++);
+	
 	for (len2 = 0; argv[2][len2]; len2++);
+	
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 
