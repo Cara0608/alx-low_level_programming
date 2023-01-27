b8cc23f68550c8b2d4a9ad51fa6fbc41b2c17bf2
Cara0608 <caraehizele0607@gmail.com>
2023-01-27 15:57:04 +0100

    Singly advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: no return
  */
 
-void _attribute_ ((constructor)) bmain()
+int main(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
