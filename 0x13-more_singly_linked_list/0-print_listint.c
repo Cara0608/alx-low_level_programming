a1f96fd1e4b10288d68250221a93333f02de526a
Cara0608 <caraehizele0607@gmail.com>
2023-01-30 10:31:07 +0100

    More singly lists

---
+++ 0x13-more_singly_linked_list/0-print_listint.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * size_t: 
+ *
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	for (size_t; i = 0; i < h ->size; i++)
+	{
+		if (h ->element[i] == NULL)
+		{
+			printf("null, not a node in list\n");
+		}
+		else
+		{
+			printf("%d", h->element[i])
+		}
+
+		count++;
+	}
+
+	return (count);
+}
