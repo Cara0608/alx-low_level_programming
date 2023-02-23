1cc7ffb6eebe0cca9e6111222f045b62e78224dc
Cara <caraehizele0607@gmail.com>
2023-02-23 10:17:15 +0100

    File

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,15 +16,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fild = open(filename, O_RDONLY);
+	
 	if (fild < 0)
 		return (0);
 
 	buffer = malloc(sizeof(char) * (letters));
+	
 	if (!buffer)
 		return (0);
 
 	chk = read(fild, buffer, letters);
 	chk = write(STDOUT_FILENO, buffer, chk);
+
 	if (chk < 0)
 		return (0);
 
