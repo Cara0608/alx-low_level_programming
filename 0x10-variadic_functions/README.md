2faa808558ac4cd29e698bcb2c67b306f5f30834
Cara Ehizele <118160982+Cara0608@users.noreply.github.com>
2023-10-24 09:16:42 +0100

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/README.md
+++
@@ -1,7 +0,0 @@
-Variadic functions
-
-0. Beauty is variable, ugliness is constant
-Writing a function that returns the sum of all parameters
-
-1. To be is to be the value of a variable
-Write a function that prints numbers followed by a new line.
--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCRIONS_
-
-int sum_them_all(const unsigned int n, ...);
-void print_strings(const char *seperator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-void print_numbers(const char *seperator, const unsigned int n, ...);
-
-#endif
